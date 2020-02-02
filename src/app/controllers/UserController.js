091c63fd2162e7e5c8c23bc141ecacbd63f778b9
Marcelo Silva <desenv.marcelo.silva@gmail.com>
2020-02-02 16:27:27 -0300

    Atualizar dados de usuário, respeitando regras de negócio.

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -23,7 +23,31 @@ class UserController {
   }
 
   async update(req, res) {
-    return res.json({ ok: req.userId });
+    const { email, oldPassword } = req.body;
+    const user = await User.findByPk(req.userId);
+
+    if (email && email !== user.email) {
+      const userExists = await User.findOne({
+        where: { email },
+      });
+
+      if (userExists) {
+        return res.status(400).json({ error: 'Usuário já existe.' });
+      }
+    }
+
+    if (oldPassword && !(await user.checkPassword(oldPassword))) {
+      return res.status(401).json({ error: 'Password não confere.' });
+    }
+
+    const { id, name, provider } = await user.update(req.body);
+
+    return res.json({
+      id,
+      name,
+      email,
+      provider,
+    });
   }
 }
 
